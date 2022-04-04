9f88242bb0536d58648d73af9c843195409fa002
jakobarsement <jakobarsement@gmail.com>
2022-04-04 09:34:52 -0500

    improve Calendar test

--- src/components/Calendar/Calendar.test.tsx
+++ src/components/Calendar/Calendar.test.tsx
@@ -1,9 +1,11 @@
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import Calendar from './Calendar'
 
 describe('<Calendar />', () => {
   it('renders the Calendar component', () => {
     const view = render(<Calendar />)
     expect(view).toBeTruthy()
+    // with the current day specified
+    expect(screen.getByText('Today')).toBeInTheDocument()
   })
 })
